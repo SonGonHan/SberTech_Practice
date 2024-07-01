03c883c14c818082001d24b5024fb7bbbb025848
SonGonHan <miskasabancik@gmail.com>
2024-07-01 18:59:44 +0300

    Задание 3

--- Task/src/Task.java
+++ Task/src/Task.java
@@ -1,40 +1,26 @@
-import java.util.Arrays;
-
 public class Task {
 
-    public static int[] generateArray(final int SIZE, final int LOWER_BOUND, final int UPPER_BOUND) {
-        int[] arr = new int[SIZE];
-        for (int i = 0; i < SIZE; ++i) {
-            arr[i] = getValueInRange(LOWER_BOUND, UPPER_BOUND);
+    public static String findPrimeNumbers(final int LOWER_BOUND, final int UPPER_BOUND) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = LOWER_BOUND; i <= UPPER_BOUND; i++) {
+            if(isPrime(i)) {
+                sb.append(i).append(" ");
+            }
         }
-        return arr;
+        return sb.toString().strip();
     }
 
-    private static int getValueInRange(int LOWER_BOUND, int UPPER_BOUND) {
-        return (int) (Math.random() * (UPPER_BOUND - LOWER_BOUND)) + LOWER_BOUND;
-    }
-
-    public static int findMinValueInArray(int[] arr) {
-        int minVal = arr[0];
-        for (int i = 1; i < arr.length; ++i) {
-            minVal = Math.min(minVal, arr[i]);
+    private static boolean isPrime(int x) {
+        for (int i = 2; i <= Math.sqrt(x); i++) {
+            if(x % i == 0) {
+                return false;
+            }
         }
-        return minVal;
-    }
-
-    public static int findMaxValueInArray(int[] arr) {
-        int maxVal = arr[0];
-        for (int i = 1; i < arr.length; ++i) {
-            maxVal = Math.max(maxVal, arr[i]);
-        }
-        return maxVal;
+        return true;
     }
 
     public static void main(String[] args) {
-        final int SIZE = 10, LOWER_BOUND = 1, UPPER_BOUND = 10;
-        int[] arr = generateArray(SIZE, LOWER_BOUND, SIZE);
-        System.out.println("Массив: " + Arrays.toString(arr));
-        System.out.println("Минимальное значение: " + findMinValueInArray(arr));
-        System.out.println("Максимальное значение: " + findMaxValueInArray(arr));
+        final int LOWER_BOUND = 2, UPPER_BOUND = 100;
+        System.out.println("Простые числа: " + findPrimeNumbers(LOWER_BOUND, UPPER_BOUND));
     }
 }
--- Task/src/TaskTest.java
+++ Task/src/TaskTest.java
@@ -3,17 +3,10 @@ import org.junit.jupiter.api.Test;
 
 class TaskTest {
     @Test
-    void GetMin() {
-        int[] array = {5, 2, 9, 1, 3};
-        int expected = 1;
-        int actual = Task.findMinValueInArray(array);
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    void GetMax() {
-        int[] array = {5, 2, 9, 1, 3};
-        int expected = 9;
-        int actual = Task.findMaxValueInArray(array);
+    void GetPrimes() {
+        final int LOWER_BOUND = 2, UPPER_BOUND = 100;
+        String expected = "2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97";
+        String actual = Task.findPrimeNumbers(LOWER_BOUND, UPPER_BOUND);
         Assert.assertEquals(expected, actual);
     }
 }
