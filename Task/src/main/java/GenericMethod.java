0074cf97d7d7c4c8ddef95d9cba3027f83fd2ad4
SonGonHan <miskasabancik@gmail.com>
2024-07-10 14:32:37 +0300

    Задание 2

--- Task/src/main/java/GenericMethod.java
+++ Task/src/main/java/GenericMethod.java
@@ -1,13 +1,10 @@
-import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
-public class GenericMethod<N extends Number> {
+public class GenericMethod {
 
-    public int countEven(Collection<N> numbers) {
-        int count = 0;
-        for (N number : numbers) {
-            count += number.intValue() % 2 == 0 ? 1 : 0;
-        }
-        return count;
+    public static void swap(List<?> list, int idx1, int idx2) {
+        Collections.swap(list, idx1, idx2);
     }
 
 }
--- Task/src/test/java/TBTest.java
+++ Task/src/test/java/TBTest.java
@@ -2,12 +2,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class TBTest {
 
     @Test
     public void test() {
-        Integer[] Nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        Assertions.assertEquals(5, new GenericMethod().countEven(Arrays.asList(Nums)));
+        Integer[] Nums1 = {1, 2, 3, 4, 5};
+        Integer[] Nums2 = {1, 4, 3, 2, 5};
+        GenericMethod.swap(Arrays.asList(Nums1), 1, 3);
+        Assertions.assertArrayEquals(Nums1, Nums2);
     }
 }
