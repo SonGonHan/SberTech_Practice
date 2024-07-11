6b35f90c2bc3b4e8b2c5ad0a0a31579349e34186
SonGonHan <miskasabancik@gmail.com>
2024-07-11 16:23:54 +0300

    Задание 3

--- Task/src/main/java/annotations/MyAnnotations.java
+++
@@ -1,9 +0,0 @@
-package annotations;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MyAnnotations {
-    MyAnnotation[] value() default {};
-}
--- Task/src/test/java/TBTest.java
+++ Task/src/test/java/TBTest.java
@@ -1,3 +1,15 @@
-public class TBTest {
+import org.junit.jupiter.api.Test;
 
+public class TBTest {
+    @Test
+    public void test() {
+        DataConteiner dc1 = new DataConteiner();
+        dc1.setAge(19);
+        dc1.setName("Миша");
+        dc1.setSurname("Шабанов");
+        DataConteiner dc2 = new DataConteiner(19, "Михаил", "Шабанов");
+        System.out.println(dc2.getAge());
+        System.out.println(dc2.getName());
+        System.out.println(dc2.getSurname());
+    }
 }
