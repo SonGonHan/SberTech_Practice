a164a426d0bb925301a0871952f3a95588d8f127
SonGonHan <miskasabancik@gmail.com>
2024-07-11 23:25:10 +0300

    Собрал все воедино

--- Task/src/main/java/StreamWork.java
+++ Task/src/main/java/StreamWork.java
@@ -1,6 +1,19 @@
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamWork {
+
+    public static void findAverageFromGeneratedNums(int bound) {
+        System.out.println(IntStream
+                .generate(()
+                        -> (int) (Math.random() * bound))
+                .limit(20)
+                .filter(x -> x > 9 && x < 100)
+                .sorted()
+                .average()
+                .orElse(0)
+        );
+    }
 
     public static Stream<Boolean> convertStringToStream(String str) {
         return Stream.of(str.split("")).map(v -> v.equals("1"));
