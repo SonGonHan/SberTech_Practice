93a7c73377553dd560d108b0b861cf6327d12ddd
SonGonHan <miskasabancik@gmail.com>
2024-07-11 21:45:52 +0300

    Задание 4

--- Task/src/main/java/WorkWithStreamContainer.java
+++ Task/src/main/java/WorkWithStreamContainer.java
@@ -1,26 +1,22 @@
+import lombok.AllArgsConstructor;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WorkWithStreamContainer {
 
-    public static void convertAndPrintStream(Stream<String> streamOfStrings) {
-        Stream<StreamContainer> streamOfStreamContainers = converterToStreamOfStreamContainers(streamOfStrings);
-        printStreamOfStreamContainers(streamOfStreamContainers);
-
+    public static List<StreamContainer> getListOfStreamContainer(Map<String, List<StreamContainer>> map){
+        return convertMapToStream(map).collect(Collectors.toList());
     }
 
-    private static void printStreamOfStreamContainers(Stream<StreamContainer> streamOfStreamContainers) {
-        streamOfStreamContainers.forEach(System.out::println);
+    public static long getSizeOfList(List<StreamContainer> list){
+        return list.stream().count();
     }
 
-    private static Stream<StreamContainer> converterToStreamOfStreamContainers(Stream<String> streamOfStrings) {
-        return streamOfStrings.map(string -> new StreamContainer(string, (long) (Math.random() * Long.MAX_VALUE)));
-    }
-
-    public static Map<Long, String> converterToMap(Stream<StreamContainer> streamOfStreamContainers) {
-        Map<Long, String> map = new HashMap<>();
-        streamOfStreamContainers.forEach(sc -> map.put(sc.getCount(), sc.getName()));
-        return map;
+    private static Stream<StreamContainer> convertMapToStream(Map<String, List<StreamContainer>> map) {
+        return map.values().stream().flatMap(List::stream);
     }
 }
--- Task/src/test/java/TBTest.java
+++ Task/src/test/java/TBTest.java
@@ -2,26 +2,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TBTest {
+
     @Test
-    public void convertAndPrint() {
-        WorkWithStreamContainer.convertAndPrintStream(Stream.of("a", "b", "c"));
+    public void testConverterToList() {
+        StreamContainer sc1 = new StreamContainer("12", 12);
+        StreamContainer sc2 = new StreamContainer("34", 34);
+        StreamContainer sc3 = new StreamContainer("56", 56);
+        StreamContainer sc4 = new StreamContainer("78", 78);
+        StreamContainer sc5 = new StreamContainer("90", 90);
+        Map<String, List<StreamContainer>> map = new HashMap<>();
+
+        map.put("1234", Stream.of(sc1, sc2).collect(Collectors.toList()));
+        map.put("5678", Stream.of(sc3, sc4).collect(Collectors.toList()));
+        map.put("90", Stream.of(sc5).collect(Collectors.toList()));
+        List<StreamContainer> lc1 = Stream.of(sc1, sc2, sc3, sc4, sc5).toList();
+        List<StreamContainer> lc2 = WorkWithStreamContainer.getListOfStreamContainer(map);
+        Assertions.assertTrue(lc1.containsAll(lc2) && lc2.containsAll(lc1));
     }
 
     @Test
-    public void testConverterToMap() {
-        StreamContainer sc1 = new StreamContainer("123", 123);
-        StreamContainer sc2 = new StreamContainer("456", 456);
-        StreamContainer sc3 = new StreamContainer("789", 789);
-        Map<Long, String> map1 = new HashMap<>();
-        map1.put(123L, "123");
-        map1.put(456L, "456");
-        map1.put(789L, "789");
-        Map<Long, String> map2 = WorkWithStreamContainer.converterToMap(Stream.of(sc1, sc2, sc3));
-        Assertions.assertEquals(map1, map2);
+    public void testSize() {
+        StreamContainer sc1 = new StreamContainer("12", 12);
+        StreamContainer sc2 = new StreamContainer("34", 34);
+        StreamContainer sc3 = new StreamContainer("56", 56);
+        StreamContainer sc4 = new StreamContainer("78", 78);
+        StreamContainer sc5 = new StreamContainer("90", 90);
+        List<StreamContainer> sc = Stream.of(sc1, sc2, sc3, sc4, sc5).toList();
+        Assertions.assertEquals(5, WorkWithStreamContainer.getSizeOfList(sc));
     }
 
 }
